dd9ccfdb524269fe06d9a1927dc75ec2febc487b
kraban <mattiasleifsson@hotmail.com>
2016-02-02 11:38:07 +0100

    Boss dodge

--- Outer Space/Outer Space/Outer Space/Boss.cs
+++ Outer Space/Outer Space/Outer Space/Boss.cs
@@ -12,17 +12,38 @@ using Microsoft.Xna.Framework.Media;
 namespace Outer_Space
 {
     public enum ChargeState { Initialize, Beginning, Charge, Finished, NotCharging }
+    public enum DodgeState { Begin, Dodge, NotDodging }
     class Boss : Enemy
     {
+        // Charge
         private ChargeState charge;
         private float accelerate;
         private Rectangle chargeRectangle { get { return new Rectangle((int)Position.X - Texture.Width / 3, (int)Position.Y - Texture.Height / 3, (int)(Texture.Width * (2f/3f)), (int)(Texture.Height * (2f/3f))); } }
+
+        // Dodge
+        private DodgeState dodge;
 
         public Boss()
             : base()
         {
             this.Texture = TextureManager.boss;
             this.charge = ChargeState.NotCharging;
+            this.dodge = DodgeState.NotDodging;
+            this.Depth = 0.5f;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+
+            if (dodge == DodgeState.Dodge)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    spriteBatch.Draw(Texture, Position, null, Color.LightGray * (0.9f - ((float)i / 5f)), Direction, new Vector2(Texture.Width / 2, Texture.Height / 2), Size + 0.2f * (float)i, SpriteEffects.None, ((float)i / 10f));
+                    spriteBatch.Draw(Texture, new Vector2((int)ShipLocation * 100 + 200, Position.Y), null, Color.LightGray * (0.9f - ((float)i / 5f)), Direction, new Vector2(Texture.Width / 2, Texture.Height / 2), 1 - Size + 0.1f * (float)i, SpriteEffects.None, ((float)i / 10f));
+                }
+            }
         }
 
         public override void UpdateLevel(Level level)
@@ -34,9 +55,46 @@ namespace Outer_Space
             }
             if (level.Started)
             {
+                // Shield
+                if (charge == ChargeState.NotCharging && dodge == DodgeState.NotDodging)
+                {
+                    bool shotAt = false;
+                    foreach (Shot shot in level.GameObjects.Where(item => item is Shot))
+                    {
+                        if (shot.Targets.Any(item => item == "Boss") && shot.Position.Y < 300 && shot.Position.X < Position.X + 10 && shot.Position.X > Position.Y - 10)
+                        {
+                            shotAt = true;
+                            break;
+                        }
+                    }
+                    if (shotAt)
+                    {
+                        dodge = DodgeState.Dodge;
+                        List<int> possibleLocations = new List<int>();
+                        for (int i = 0; i < 3; i++)
+                        {
+                            if (i != (int)ShipLocation)
+                            {
+                                possibleLocations.Add(i);
+                            }
+                        }
+                        ShipLocation = (Location)possibleLocations[Globals.Randomizer.Next(0, possibleLocations.Count())];
+                    }
+                }
+                if (dodge == DodgeState.Dodge)
+                {
+                    Size = MathHelper.Lerp(Size, 0, 0.1f);
+                    if (Size < 0.1f)
+                    {
+                        dodge = DodgeState.NotDodging;
+                        Position = new Vector2((int)ShipLocation * 100 + 200, Position.Y);
+                        Size = 1;
+                    }
+                }
+
                 // Start charging
                 // Will only charge is the player has a chance to avoid it
-                if (charge == ChargeState.NotCharging && Globals.Randomizer.Next(0, 1001) < 3 && level.CheckPossibleMatches().Any(item => item.Type == TileType.left || item.Type == TileType.right || item.Type == TileType.shoot))
+                if (charge == ChargeState.NotCharging && dodge == DodgeState.NotDodging && Globals.Randomizer.Next(0, 1001) < 0 && level.CheckPossibleMatches().Any(item => item.Type == TileType.left || item.Type == TileType.right || item.Type == TileType.shoot))
                 {
                     charge = ChargeState.Initialize;
                 }
@@ -84,6 +142,17 @@ namespace Outer_Space
                     Camera.ScreenShakeTimer = 30;
                 }
 
+                // Shot hit
+                if (charge != ChargeState.NotCharging)
+                {
+                    foreach (Shot shot in level.GameObjects.Where(item => item is Shot))
+                    {
+                        if (shot.Targets.Any(item => item == "Boss") && shot.Box.Intersects(Box))
+                        {
+                            charge = ChargeState.Finished;
+                        }
+                    }
+                }
                 // Miss
                 if (Position.Y > Globals.ScreenSize.Y)
                 {
